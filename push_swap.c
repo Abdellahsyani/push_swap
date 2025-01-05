2cda3a1c00b5be350a132e884b4052775fc770ff
Abdellahsyani <syaniabdos@gmail.com>
2025-01-05 16:20:06 +0100

    add all

---
+++ push_swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int	main(int ac, char *av)
+{
+	int	*stack_a;
+	int	*stack_b;
+	int	i = 0;
+	int 	j = 0;
+
+	if (ac > 1)
+	{
+		int	len = strlen(ac);
+		int	pivot = av[0];
+		stack_a = malloc(sizeof(len));
+		while (av[i])
+		{
+		}
+	}
+}
