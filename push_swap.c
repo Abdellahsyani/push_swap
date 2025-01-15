b5ca8250295a332b3b0ab502cb71591f4f0cacd9
Abdellahsyani <syaniabdos@gmail.com>
2025-01-15 18:54:51 +0100

    add

--- push_swap.c
+++ push_swap.c
@@ -63,7 +63,47 @@ void	quick_sort_stack(t_list **stack_a, t_list **stack_b, int size)
 	while (*stack_b)
 		pa(stack_a, stack_b);
 }
+static	char	*helper(char *concat, char const *s1, char const *s2)
+{
+	int	i;
+	int	j;
 
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		concat[i + j] = s2[j];
+		j++;
+	}
+	concat[i + j] = '\0';
+	return (concat);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*concat;
+	int		s1_size;
+	int		s2_size;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
+	s1_size = ft_strlen(s1);
+	s2_size = ft_strlen(s2);
+	concat = malloc(sizeof(char) * (s1_size + s2_size) + 1);
+	if (!concat)
+		return (NULL);
+	helper(concat, s1, s2);
+	return (concat);
+}
 
 int main(int ac, char **av)
 {
