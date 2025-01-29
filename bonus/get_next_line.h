df6f33512251bed9e36d49bd5fd48c04c9f562e6
Abdellahsyani <syaniabdos@gmail.com>
2025-01-29 09:52:41 +0100

    fixed

--- bonus/get_next_line.h
+++ bonus/get_next_line.h
@@ -31,6 +31,6 @@ void	ft_strcat(char *concat, char *s1, char *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(char *s);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
--- push_swap.h
+++ push_swap.h
@@ -26,9 +26,6 @@ typedef struct n_list
 	int				index;
 }					t_list;
 
-# define MAX_INT 2147483647
-# define MIN_INT -2147483648
-
 /*helper functions to sort stack*/
 void				max_min(long num, char **dup);
 int					count_elements(t_list *stack);
@@ -41,7 +38,6 @@ char				**ft_split(char const *s, char c);
 void				fill_stack_b(t_list **stack_a, t_list **stack_b, int num_c,
 						int ch_s);
 int					count_nodes(t_list *stack);
-//int					count_min(t_list **stack_a, int min_range, int max_range);
 void				verify_dup(char **av);
 int					check_dup(t_list *stack_a);
 t_list				*verify_stack(t_list *stack_a, int ac, char **av);
